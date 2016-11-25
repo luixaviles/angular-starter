19cad07a381f5f4515f28a954ed3903809feb466
luixaviles <luis.avilesz@gmail.com>
2016-11-25 01:15:46 -0400

    Adding Speaker model and input bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Speaker} from './index'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  speaker: Speaker;
+
+  ngOnInit() {
+    this.initModel();
+  }
+
+  initModel() {
+    this.speaker = new Speaker('', '');
+  }
+
 }
